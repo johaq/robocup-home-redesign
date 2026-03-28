c1ee6ab3c9cffc697ea6efa81c8a1985d66745c9
johaq <jkummert@techfak.uni-bielefeld.de>
2026-03-28 14:04:57 +0100

    header and footer path fix try again again

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,6 @@ async function loadComponent(id, file) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  loadComponent("navbar", "../../components/navbar.html");
-  loadComponent("footer", "../../components/footer.html");
+  loadComponent("navbar", "/components/navbar.html");
+  loadComponent("footer", "/components/footer.html");
 });
